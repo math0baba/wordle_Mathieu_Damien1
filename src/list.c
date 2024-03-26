c824b28b6b28e26316504e68c79546815d222903
MathBaba <mathieubaba492@gmail.com>
2024-03-26 10:39:50 +0400

    code version 1

---
+++ src/list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    
+    string* Tab (){
+        string* Tab[1000*1000]
+    }
+    
+    
+    FILE*fichier=NULL;
+    FILE*fichier=fopen("../ressource/bdd_wordle.txt","r");
+    
+    char str[6];
+    
+    if (fichier != NULL){
+        printf ("le fichier a bien été ouvert\n");
+    } else{
+        printf ("problème d'ouverture");
+    }
+}
